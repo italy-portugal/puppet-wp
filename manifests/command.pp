3472237037b31c4ae48609d57ed8cfd160ab2398
Jonathan Camara <jcamara@ifma.org>
2015-10-07 17:09:41 -0500

    added onlyif parameter and fixed linting errors onlyif parameter is so that the command only runs when it needs to.

--- manifests/command.pp
+++ manifests/command.pp
@@ -1,14 +1,16 @@
+#
 define wp::command (
-	$location,
-	$command
+  $location,
+  $command,
+  $onlyif = '/usr/bin/wp --version'
 ) {
-	include wp::cli
+  include wp::cli
 
-	exec {"$location wp $command":
-		command => "/usr/bin/wp $command",
-		cwd => $location,
-		user => $::wp::user,
-		require => [ Class['wp::cli'] ],
-		onlyif => '/usr/bin/wp core is-installed'
-	}
+  exec {"${location} wp ${command}":
+    command => '/usr/bin/wp $command',
+    cwd     => $location,
+    user    => $::wp::user,
+    require => [ Class['wp::cli'] ],
+    onlyif  => ['/usr/bin/wp core is-installed',$onlyif]
+  }
 }
