336a86e45d56981440f8ddbd45bab68937ddbe68
Jonathan Camara <jcamara@ifma.org>
2015-10-07 17:02:11 -0500

    fixed all the linting errors

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -1,41 +1,42 @@
+#
 define wp::plugin (
-	$slug = $title,
-	$location,
-	$ensure = 'enabled',
-	$networkwide = false
+  $location,
+  $slug = $title,
+  $ensure = 'enabled',
+  $networkwide = false
 ) {
-	include wp::cli
+  include wp::cli
 
-	case $ensure {
-		'enabled': {
+  case $ensure {
+    'enabled': {
 
-			exec { "wp install plugin $title":
-				cwd     => $location,
-				command => "/usr/bin/wp plugin install $slug",
-				unless  => "/usr/bin/wp plugin is-installed $slug",
-				before  => Wp::Command["$location plugin $slug $ensure"],
-				require => Class["wp::cli"],
-				onlyif  => "/usr/bin/wp core is-installed",
-			}
+      exec { "wp install plugin ${title}":
+        cwd     => $location,
+        command => "/usr/bin/wp plugin install ${slug}",
+        unless  => "/usr/bin/wp plugin is-installed ${slug}",
+        before  => Wp::Command["${location} plugin ${slug} ${ensure}"],
+        require => Class['wp::cli'],
+        onlyif  => '/usr/bin/wp core is-installed',
+      }
 
-			$command = "install $slug --activate"
-		}
-		'disabled': {
-			$command = "deactivate $slug"
-		}
-		default: {
-			fail("Invalid ensure for wp::plugin")
-		}
-	}
+      $command = "install ${slug} --activate"
+    }
+    'disabled': {
+      $command = "deactivate ${slug}"
+    }
+    default: {
+      fail('Invalid ensure for wp::plugin')
+    }
+  }
 
-	if $networkwide {
-		$args = "plugin $command --network"
-	}
-	else {
-		$args = "plugin $command"
-	}
-	wp::command { "$location plugin $slug $ensure":
-		location => $location,
-		command => $args
-	}
+  if $networkwide {
+    $args = "plugin ${command} --network"
+  }
+  else {
+    $args = "plugin ${command}"
+  }
+  wp::command { "${location} plugin ${slug} ${ensure}":
+    location => $location,
+    command  => $args,
+  }
 }
