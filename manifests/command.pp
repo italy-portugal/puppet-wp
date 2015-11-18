0f77233e5df1b23395f5e6dbe7c0842cfccebeea
Jonathan Camara <jcamara@ifma.org>
2015-11-18 15:15:59 -0600

    wp::command should only run when it is refreshed.

--- manifests/command.pp
+++ manifests/command.pp
@@ -9,6 +9,7 @@ define wp::command (
 		cwd => $location,
 		user => $::wp::user,
 		require => [ Class['wp::cli'] ],
-		onlyif => '/usr/bin/wp core is-installed'
+		onlyif => '/usr/bin/wp core is-installed',
+		refreshonly => true
 	}
 }
